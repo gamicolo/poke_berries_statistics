1d007a7d857cc5c332ea80efab9e6bff1d984fd0
gamicolo <guillermoamicolo@gmail.com>
2022-05-13 11:21:24 -0300

    feat:group tests in classes for consistency

--- test/test_berry_statistics.py
+++ test/test_berry_statistics.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pytest
 
 import pytest
 
@@ -6,78 +6,106 @@ import os
 import sys
 sys.path.append('../src')
 
+import aiohttp
 import poke_stats_api 
 
-def test_berry_statistics_success():
+class TestGetBerryStatistics():
 
-    r = [{'name': 'cheri', 'growth_time': 3}, {'name': 'chesto', 'growth_time': 1}, {'name': 'pecha', 'growth_time': 8}, {'name': 'lum', 'growth_time': 12}]
+    def test_berry_statistics_success(self):
 
-    stats = poke_stats_api.Statistics()
-    result = stats._get_berry_statistics(r)
+        r = [{'name': 'cheri', 'growth_time': 3}, {'name': 'chesto', 'growth_time': 1}, {'name': 'pecha', 'growth_time': 8}, {'name': 'lum', 'growth_time': 12}]
 
-    assert result['berries_names'] == ['cheri', 'chesto', 'pecha', 'lum']
-    assert result['min_growth_time'] == 1
-    assert result['max_growth_time'] == 12
-    assert result['mean_growth_time'] == 6
-    assert result['median_growth_time'] == 5.5
-    assert result['variance_growth_time'] == 24.666666666666668
+        stats = poke_stats_api.Statistics()
+        result = stats._get_berry_statistics(r)
 
-def test_berry_statistics_with_empty_berry_data():
+        assert result['berries_names'] == ['cheri', 'chesto', 'pecha', 'lum']
+        assert result['min_growth_time'] == 1
+        assert result['max_growth_time'] == 12
+        assert result['mean_growth_time'] == 6
+        assert result['median_growth_time'] == 5.5
+        assert result['variance_growth_time'] == 24.666666666666668
 
-    stats = poke_stats_api.Statistics()
-    result = stats._get_berry_statistics([])
+    def test_berry_statistics_with_empty_berry_data(self):
 
-    assert result['berries_names'] == []
-    assert result['min_growth_time'] == 0
-    assert result['max_growth_time'] == 0
-    assert result['mean_growth_time'] == 0
-    assert result['median_growth_time'] == 0
-    assert result['variance_growth_time'] == 0
+        stats = poke_stats_api.Statistics()
+        result = stats._get_berry_statistics([])
 
-
-def test_berry_statistics_with_missing_name_key_on_berry_data():
-
-    r = [{'name': 'cheri', 'growth_time': 3}, {'name': 'chesto'}, {'name': 'pecha', 'growth_time': 8}, {'name': 'lum', 'growth_time': 12}]
-
-    stats = poke_stats_api.Statistics()
-    result = stats._get_berry_statistics(r)
-
-    assert result['berries_names'] == ['cheri', 'pecha', 'lum']
-    assert result['min_growth_time'] == 3
-    assert result['max_growth_time'] == 12
-    assert result['mean_growth_time'] == 7.666666666666667
-    assert result['median_growth_time'] == 8
-    assert result['variance_growth_time'] == 20.333333333333336
+        assert result['berries_names'] == []
+        assert result['min_growth_time'] == 0
+        assert result['max_growth_time'] == 0
+        assert result['mean_growth_time'] == 0
+        assert result['median_growth_time'] == 0
+        assert result['variance_growth_time'] == 0
 
 
-def test_berry_statistics_with_missing_growth_time_key_on_berry_data():
+    def test_berry_statistics_with_missing_name_key_on_berry_data(self):
 
-    r = [{'name': 'cheri', 'growth_time': 3}, {'growth_time': 1}, {'name': 'pecha', 'growth_time': 8}, {'name': 'lum', 'growth_time': 12}]
+        r = [{'name': 'cheri', 'growth_time': 3}, {'name': 'chesto'}, {'name': 'pecha', 'growth_time': 8}, {'name': 'lum', 'growth_time': 12}]
 
-    stats = poke_stats_api.Statistics()
-    result = stats._get_berry_statistics(r)
+        stats = poke_stats_api.Statistics()
+        result = stats._get_berry_statistics(r)
 
-    assert result['berries_names'] == ['cheri', 'pecha', 'lum']
-    assert result['min_growth_time'] == 3
-    assert result['max_growth_time'] == 12
-    assert result['mean_growth_time'] == 7.666666666666667
-    assert result['median_growth_time'] == 8
-    assert result['variance_growth_time'] == 20.333333333333336
+        assert result['berries_names'] == ['cheri', 'pecha', 'lum']
+        assert result['min_growth_time'] == 3
+        assert result['max_growth_time'] == 12
+        assert result['mean_growth_time'] == 7.666666666666667
+        assert result['median_growth_time'] == 8
+        assert result['variance_growth_time'] == 20.333333333333336
 
-def test_berry_statistics_frequency_growth_time_berries_with_single_frecuency():
 
-    r = [{'name': 'cheri', 'growth_time': 3}, {'name': 'pecha', 'growth_time': 8}, {'name': 'lum', 'growth_time': 12}]
+    def test_berry_statistics_with_missing_growth_time_key_on_berry_data(self):
 
-    stats = poke_stats_api.Statistics()
-    result = stats._get_berry_statistics(r)
+        r = [{'name': 'cheri', 'growth_time': 3}, {'growth_time': 1}, {'name': 'pecha', 'growth_time': 8}, {'name': 'lum', 'growth_time': 12}]
 
-    assert result['frequency_growth_time'] == { 3: 1, 8: 1, 12: 1 }
+        stats = poke_stats_api.Statistics()
+        result = stats._get_berry_statistics(r)
 
-def test_berry_statistics_frequency_growth_time_berries_without_single_frecuency():
+        assert result['berries_names'] == ['cheri', 'pecha', 'lum']
+        assert result['min_growth_time'] == 3
+        assert result['max_growth_time'] == 12
+        assert result['mean_growth_time'] == 7.666666666666667
+        assert result['median_growth_time'] == 8
+        assert result['variance_growth_time'] == 20.333333333333336
 
-    r = [{'name': 'cheri', 'growth_time': 3}, {'name': 'pecha', 'growth_time': 3}, {'name': 'lum', 'growth_time': 12}]
+    def test_berry_statistics_frequency_growth_time_berries_with_single_frecuency(self):
 
-    stats = poke_stats_api.Statistics()
-    result = stats._get_berry_statistics(r)
+        r = [{'name': 'cheri', 'growth_time': 3}, {'name': 'pecha', 'growth_time': 8}, {'name': 'lum', 'growth_time': 12}]
 
-    assert result['frequency_growth_time'] == { 3: 2, 12: 1 }
+        stats = poke_stats_api.Statistics()
+        result = stats._get_berry_statistics(r)
+
+        assert result['frequency_growth_time'] == { 3: 1, 8: 1, 12: 1 }
+
+    def test_berry_statistics_frequency_growth_time_berries_without_single_frecuency(self):
+
+        r = [{'name': 'cheri', 'growth_time': 3}, {'name': 'pecha', 'growth_time': 3}, {'name': 'lum', 'growth_time': 12}]
+
+        stats = poke_stats_api.Statistics()
+        result = stats._get_berry_statistics(r)
+
+        assert result['frequency_growth_time'] == { 3: 2, 12: 1 }
+
+class TestGetBerriesData:
+
+    @pytest.mark.asyncio
+    async def test_async_get_berries_data_empty_urls(self):
+
+        urls = []
+        r = await poke_stats_api.get_berries_data(urls)
+
+        assert r == []
+
+class TestGetBerryData:
+
+    @pytest.mark.asyncio
+    async def test_async_get_berry_data_empty_url(self):
+
+        url = ''
+        r = await poke_stats_api.get_berry_data(aiohttp.ClientSession(), url)
+
+        assert r == {}
+
+        #async with aiohttp.ClientSession() as session:
+        #    r = await poke_stats_api.get_berry_data(session, url)
+
+
